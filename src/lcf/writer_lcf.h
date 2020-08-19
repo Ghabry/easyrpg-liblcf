0a030ca836ce28ce47291a18a296aad2cd88c56b
Matthew Fioravante <fmatthew5876@gmail.com>
2020-08-18 22:47:05 -0400

    Writer_Lcf support DBArray

--- src/lcf/writer_lcf.h
+++ src/lcf/writer_lcf.h
@@ -12,6 +12,7 @@
 
 #include "lcf/config.h"
 #include "lcf/dbbitarray.h"
+#include "lcf/dbarray.h"
 #include "lcf/dbstring.h"
 
 #include <string>
@@ -96,6 +97,14 @@ public:
 	 */
 	template <class T>
 	void Write(const std::vector<T>& buffer);
+
+	/**
+	 * Write a DBArray of primitive values to the stream.
+	 *
+	 * @param buffer DBArray to write.
+	 */
+	template <class T>
+	void Write(const DBArray<T>& buffer);
 
 	/**
 	 * Returns the current position of the read pointer in
